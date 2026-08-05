da39f85cdcee88d8c0b5e362bc07d849d304b510
Maha Arshad <mahaarshad634@gmail.com>
2026-08-05 15:13:42 +0500

    firebas config

--- reactwithfirebase/src/context/firebase.jsx
+++ reactwithfirebase/src/context/firebase.jsx
@@ -1,6 +1,16 @@
 import {createContext, useContext, useEffect, useState} from 'react';
 import {intializeApp} from 'firebase/app';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDA1y8BrUIMqKFbp_ZDrSFuvEotbtd_5Ts",
+  authDomain: "my-app-f4b18.firebaseapp.com",
+  databaseURL: "https://my-app-f4b18-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "my-app-f4b18",
+  storageBucket: "my-app-f4b18.firebasestorage.app",
+  messagingSenderId: "443448293625",
+  appId: "1:443448293625:web:ebd3c5f403c60c5ca50a28"
+};
+
 const firebaseapp=initializeApp({});
 
 const FirebaseContext = createContext(null);
