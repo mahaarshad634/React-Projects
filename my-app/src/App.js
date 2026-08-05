ebf779c9442ff8d7168882803a9be6528748c1b3
Maha Arshad <mahaarshad634@gmail.com>
2026-08-05 13:32:21 +0500

    add a user authenticate

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,25 @@
-import {getDatabase, ref, set} from 'firebase/database';
+//import {getDatabase, ref, set} from 'firebase/database';
+import {getAuth , createUserWithEmailAndPassword} from 'firebase/auth';
 import { app } from './firebase';
 import './App.css';
+
+const auth = getAuth(app);
  
-const db=getDatabase(app);
-const putdata=()=>{
-  set(ref(db,'users/1'),{
-    username:'John Doe',
-    email:'john.doe@example.com'
-  });
-};
+// const db=getDatabase(app);
+// const putdata=()=>{
+//   set(ref(db,'users/maha'),{
+//     username:'maha arshad',
+//     email:'maha@example.com'
+//   });
+// };
 
 function App() {
+  const  signUpUser=()=>{
+    createUserWithEmailAndPassword(auth,'mahaarshad123@example.com','maha123').then((value)=> console.log(value))};
   return (
     <div className="App">
      <h1>Welcome to My App with firebase</h1>
-     <button onClick={putdata}>Put Data</button>
+     <button onClick={signUpUser}>Sign Up</button>
     </div>
   );
 }
