55bc5254d5b91ec52c741c38970c1ff729f84f46
Maha Arshad <mahaarshad634@gmail.com>
2026-08-06 18:25:18 +0500

    write data in firestore

--- firestoreprj/src/App.jsx
+++ firestoreprj/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
-
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
 import './App.css'
+import { app } from './firebase'
+
+const firestore = getFirestore(app)
 
 function App() {
-  const [count, setCount] = useState(0)
+ const writeData = async () => {
+  const result = await addDoc(collection(firestore, 'cities'), {
+    name: 'Lahore',
+    pinCode: 54000,
+    country: 'Pakistan'
+  });
+  console.log('Result:', result);
+ }
 
   return (
     <>
-     
+     <h1>Vite + React firestore</h1>
+     <button onClick={writeData}>Write Data</button>
 
     </>
   )
---
+++ firestoreprj/src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const currentHost = typeof window !== 'undefined' ? window.location.hostname : 'localhost';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDA1y8BrUIMqKFbp_ZDrSFuvEotbtd_5Ts",
+  authDomain: currentHost === 'localhost' ? 'localhost' : 'my-app-f4b18.firebaseapp.com',
+  databaseURL: "https://my-app-f4b18-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "my-app-f4b18",
+  storageBucket: "my-app-f4b18.firebasestorage.app",
+  messagingSenderId: "443448293625",
+  appId: "1:443448293625:web:233a6b93f6244887a50a28"
+};
+
+export const app = initializeApp(firebaseConfig);
