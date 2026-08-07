68fa10b31f5b123fac3cdc1ce0bbf9aad3fc33f9
Maha Arshad <mahaarshad634@gmail.com>
2026-08-07 20:59:52 +0500

    change nav

--- bookify/src/Components/navbar.jsx
+++ bookify/src/Components/navbar.jsx
@@ -9,9 +9,8 @@ const MyNav =()=>{
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="">Home</Nav.Link>
+             <Nav.Link href="">Add Listing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
